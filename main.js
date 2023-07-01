b3b17040fb3639557e6c83b587310925657f0386
nhuthao0511 <135961628+nhuthao0511@users.noreply.github.com>
2023-07-01 14:47:10 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -51,19 +51,13 @@ const sr = ScrollReveal ({
         reset: true
 });
 sr.reveal('.home-text',{lelay:200, origin:'top'});
-sr.reveal('.section__product',{lelay:600, origin:'left'});
-sr.reveal('.heading',{lelay:600, origin:'top'});
-sr.reveal('.heading_blog h1',{lelay:600, origin:'top'});
-sr.reveal('.ride-container .box',{lelay:800, origin:'top'});
-sr.reveal('.blog-container',{lelay:400, origin:'left'});
-sr.reveal('#backtop',{lelay:400, origin:'right'});
-sr.reveal('#backtop_booking',{lelay:400, origin:'right'});
-
-
-// --------------------------
-// for (var i = 1; i<=100; i++){
-//     console.log(i);
-// }
+sr.reveal('.section__product',{lelay:400, origin:'left'});
+sr.reveal('.heading',{lelay:400, origin:'top'});
+sr.reveal('.heading_blog h1',{lelay:400, origin:'top'});
+sr.reveal('.ride-container .box',{lelay:400, origin:'top'});
+sr.reveal('.blog-container',{lelay:600, origin:'left'});
+sr.reveal('#backtop',{lelay:600, origin:'right'});
+sr.reveal('#backtop_booking',{lelay:600, origin:'right'});
 
 
 const wrapper = document.querySelector(".blog");
@@ -129,7 +123,7 @@ const infiniteScroll = () => {
 const autoPlay = () => {
     if(window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
     // Autoplay the carousel after every 2500 ms
-    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
+    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2000);
 }
 autoPlay();
 carousel.addEventListener("mousedown", dragStart);
