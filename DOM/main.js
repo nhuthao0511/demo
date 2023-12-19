e51c419357272c253d7f826fcfc670a3c6ed43cc
nhuthao0511 <135961628+nhuthao0511@users.noreply.github.com>
2023-12-19 14:59:57 +0700

    Add files via upload

---
+++ DOM/main.js
@@ -0,0 +1,17 @@
+// console.log(document)
+
+
+// document.write('Chào các cậu nha <3')
+
+// var i = document.getElementsByClassName('u')
+// console.log(i)
+
+/**
+ * Sử dụng các biến đã cho sẵn dưới đây
+ */
+
+var productsListElement = document.querySelector('.products-list')
+var firstProductElement = document.querySelectorAll('.product')[0]
+var buttonElements = document.querySelectorAll('Button')
+
+
